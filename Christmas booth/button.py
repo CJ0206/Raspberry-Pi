b24cecb28b5e6ba96be80b36f0414a77a939ac14
Christian James-Watkins <CJ0206@users.noreply.github.com>
2018-12-07 20:57:00 +0000

    Create button.py

---
+++ Christmas booth/button.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import time
+import subprocess
+import os
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+while True:
+	input_state = GPIO.input(26)
+
+	if input_state == True:
+        	os.system('fswebcam -r 1280x720 -D 5 --no-banner --overlay christmas.png /home/pi/webcam/%d-%m-%Y_%H:%M:%S.jpg -S 2')
+        	time.sleep(5)
+
+message = input("Press enter to quit") # Run until someone presses enter
+GPIO.cleanup() # Clean up
