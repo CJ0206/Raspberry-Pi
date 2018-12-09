3e12749daca1d114530f67471ab0beb3e541d15d
Christian James-Watkins <CJ0206@users.noreply.github.com>
2018-12-09 13:28:12 +0000

    Update prototype_booth.py

--- Christmas booth/prototype_booth.py
+++ Christmas booth/prototype_booth.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python
-import gpiozero
+from gpiozero import PWMLED
+from gpiozero import Button
 from signal import pause
 import os
 import time
 
 # Define take photo
 def take_photo():
-    os.system('fswebcam -r 640x480 -D 5 --no-banner --overlay christmas.png /home/pi/webcam/%d-%m-%Y_%H:%M:%S.jpg -S 2')
-    time.sleep(5)
- 
+        os.system('fswebcam -r 640x480 -D 5 --no-banner --overlay christmas.png /home/pi/webcam/%d-%m-%Y_%H:%M:%S.jpg -S 2')
+        os.system("echo '\033[1mYour photo has been saved!'")
+        time.sleep(5)
+
 # Define LED and Button pins | Type "pinout" into the terminal to find your pin numbers
 led = PWMLED(15)
 button = Button(7)
