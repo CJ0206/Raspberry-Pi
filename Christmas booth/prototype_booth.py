f43dc6dd5d4a104b83f04ee16e5ab1e20e25cbe0
Christian James-Watkins <CJ0206@users.noreply.github.com>
2018-12-09 14:41:33 +0000

    Update prototype_booth.py

--- Christmas booth/prototype_booth.py
+++ Christmas booth/prototype_booth.py
@@ -8,12 +8,18 @@ import time
 # Define take photo
 def take_photo():
         os.system('fswebcam -r 640x480 -D 5 --no-banner --overlay christmas.png /home/pi/webcam/%d-%m-%Y_%H:%M:%S.jpg -S 2')
-        os.system("echo '\033[1mYour photo has been saved!'")
-        time.sleep(5)
+        os.system("echo '\n\n\n\n\n\n\033[1mYour photo has been saved!\n\033[00mYour photo will be available from \033[1m WEBSITE \033[00m tonight.")
+        time.sleep(10)
+        os.system('clear')
+        os.system("echo '\033[1mPush button to take photograph\033[00m'")
 
 # Define LED and Button pins | Type "pinout" into the terminal to find your pin numbers
 led = PWMLED(15)
 button = Button(7)
+
+# Instructions
+os.system('clear')
+os.system("echo '\033[1mPush button to take photograp\033[00m'")
 
 # Pulse LED and take photo when Button is pressed
 led.pulse()
